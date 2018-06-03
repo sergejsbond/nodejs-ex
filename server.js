a53f802a7aec29dbf0070fff784ca2d257e07386
Sergey <bondarenko.sergejs@gmail.com>
2018-06-03 16:29:51 +0300

    disable process

--- server.js
+++ server.js
@@ -56,10 +56,6 @@ var initDb = function(callback) {
   });
 };
 
-app.get('/process', function (req, res) {
-  res.json(process.env);
-});
-
 app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
